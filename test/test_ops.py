ed82a6413df9183896f704917a0818ae2465c328
wallace <wallacecpg@gmail.com>
2024-07-29 10:26:53 +0100

    remove test wrong

--- test/test_ops.py
+++ test/test_ops.py
@@ -13,6 +13,6 @@ def test_divide():
 def test_substract():
     assert substract(10, 5) == 5
 
-def test_multiply_wrong():
-    assert multiply(2, 3) == 7
+#def test_multiply_wrong():
+#    assert multiply(2, 3) == 7
 
